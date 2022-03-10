a70eeab17c033fbe09e0ddd71e5d169a7a02b041
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-03-10 12:08:23 +0100

    feat(color): add support to xterm 256 colors.

--- pkg/color/color.go
+++ pkg/color/color.go
@@ -4,38 +4,19 @@ import (
 	"github.com/hinshun/vt10x"
 )
 
-//nolint
-var ansiColors = []string{
-	"#000000", // Black
-	"#800000", // Red
-	"#008000", // Green
-	"#808000", // Yellow
-	"#000080", // Blue
-	"#800080", // Magent
-	"#008080", // Cyan
-	"#c0c0c0", // Grey
-
-	"#808080", // Dark Grey
-	"#ff0000", // Light Red
-	"#00ff00", // Light Green
-	"#ffff00", // Light Yellow
-	"#0000ff", // Light Blue
-	"#ff00ff", // Light Magent
-	"#00ffff", // Light Cyan
-	"#ffffff", // White
-}
-
-func ansiToColor(ansi uint32) string {
-	return ansiColors[ansi]
-}
+//go:generate go run colorsgen.go
 
 func GetColor(c vt10x.Color) string {
 	var colorStr string
 
+	if c == vt10x.DefaultFG {
+		return ansiColors[int(vt10x.LightGrey)]
+	}
+
 	if c.ANSI() {
-		colorStr = ansiToColor(uint32(c))
+		colorStr = ansiColors[int(c)]
 	} else {
-		colorStr = ansiToColor(uint32(vt10x.LightGrey))
+		colorStr = xtermColors[int(c)]
 	}
 
 	return colorStr
