f108c8bbc8bbb4e09fc1fe2eb2c37df8c4dc995e
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-03-12 14:41:59 +0100

    feat(export): add minify option

--- cmd/termsvg/export/export.go
+++ cmd/termsvg/export/export.go
@@ -1,16 +1,20 @@
 package export
 
 import (
+	"bytes"
 	"os"
 
 	"github.com/mrmarble/termsvg/internal/svg"
 	"github.com/mrmarble/termsvg/pkg/asciicast"
 	"github.com/rs/zerolog/log"
+	"github.com/tdewolff/minify/v2"
+	msvg "github.com/tdewolff/minify/v2/svg"
 )
 
 type Cmd struct {
 	Input  string `short:"i" type:"existingfile" help:"asciicast file to export"`
-	Output string `optional:"" short:"o" type:"path" help:"where to save the file"`
+	Output string `optional:"" short:"o" type:"path" help:"where to save the file. Defaults to <input>.svg"`
+	Mini   bool   `name:"minify" optional:"" short:"m" help:"minify output file. May be slower"`
 }
 
 func (cmd *Cmd) Run() error {
@@ -19,7 +23,7 @@ func (cmd *Cmd) Run() error {
 		output = cmd.Input + ".svg"
 	}
 
-	err := export(cmd.Input, output)
+	err := export(cmd.Input, output, cmd.Mini)
 	if err != nil {
 		return err
 	}
@@ -29,7 +33,7 @@ func (cmd *Cmd) Run() error {
 	return nil
 }
 
-func export(input, output string) error {
+func export(input, output string, mini bool) error {
 	inputFile, err := os.ReadFile(input)
 	if err != nil {
 		return err
@@ -44,10 +48,27 @@ func export(input, output string) error {
 	if err != nil {
 		return err
 	}
-
 	defer outputFile.Close()
 
-	svg.Export(*cast, outputFile)
+	if mini {
+		out := new(bytes.Buffer)
+		svg.Export(*cast, out)
+
+		m := minify.New()
+		m.AddFunc("image/svg+xml", msvg.Minify)
+
+		b, err := m.Bytes("image/svg+xml", out.Bytes())
+		if err != nil {
+			return err
+		}
+
+		_, err = outputFile.Write(b)
+		if err != nil {
+			return err
+		}
+	} else {
+		svg.Export(*cast, outputFile)
+	}
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/creack/pty v1.1.17
 	github.com/hinshun/vt10x v0.0.0-20220301184237-5011da428d02
 	github.com/sebdah/goldie/v2 v2.5.3
+	github.com/tdewolff/minify/v2 v2.10.0
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
 )
 
@@ -14,6 +15,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/tdewolff/parse/v2 v2.5.27 // indirect
 )
 
 require (
