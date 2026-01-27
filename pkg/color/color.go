86e9e860a662121d09a9179e009f3ccb1c00884c
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2026-01-27 23:08:36 +0100

    feat: add Color and Palette types for terminal color representation

---
+++ pkg/color/color.go
@@ -0,0 +1,53 @@
+package color
+
+import (
+	"fmt"
+	"image/color"
+)
+
+// Color represents a terminal color.
+type Color struct {
+	// Type indicates how to interpret the color.
+	Type  ColorType
+	Index uint8      // For ANSI/Extended (0-255) colors
+	RGB   color.RGBA // For TrueColor colors
+}
+
+type ColorType uint8
+
+const (
+	Default ColorType = iota
+	ANSI
+	Extended
+	TrueColor
+)
+
+func FromANSI(index uint8) Color {
+	return Color{Type: ANSI, Index: index}
+}
+
+func FromExtended(index uint8) Color {
+	return Color{Type: Extended, Index: index}
+}
+
+func FromRGB(r, g, b uint8) Color {
+	return Color{Type: TrueColor, RGB: color.RGBA{R: r, G: g, B: b, A: 255}}
+}
+
+// ToRGBA converts the Color to an RGBA value using the palette.
+func (c Color) ToRGBA(palette Palette) color.RGBA {
+	switch c.Type {
+	case ANSI, Extended:
+		return palette.At(c.Index)
+	case TrueColor:
+		return c.RGB
+	default:
+		return color.RGBA{0, 0, 0, 0} // Transparent for Default
+	}
+}
+
+// ToHex returns the color as a hex string (e.g., "#RRGGBB").
+func (c Color) ToHex(palette Palette) string {
+	rgba := c.ToRGBA(palette)
+	return fmt.Sprintf("#%02X%02X%02X", rgba.R, rgba.G, rgba.B)
+}
